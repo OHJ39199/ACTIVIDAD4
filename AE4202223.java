fd17fbe5ff1b14e9b86b1f3743482ec1cfbd8b77
Andres_Caso <OHJ39199@educastur.es>
2023-03-24 11:49:52 +0100

    Inclusion de comentarios

--- AE4202223.java
+++ AE4202223.java
@@ -1,6 +1,14 @@
 package calculadora;
 import java.util.Scanner;
 
+//-----------------------------------
+//Clase: AE4202223 
+//Fecha: 23/03/2023	
+//Autor: Andres Caso Iglesias
+//Comentario: programa que realiza las operaciones básicas matemáticas partiendo de dos numeros enteros
+//(suma, resta, multiplicación y división)
+//-----------------------------------
+
 public class AE4202223 {
 
 	public static void main(String[] args) {
@@ -13,12 +21,14 @@ public class AE4202223 {
         System.out.print("Introduzca el segundo numero entero positivo: ");
         int num2 = input.nextInt();
 
+        // Realizar las operaciones matemáticas básicas
         int suma = num1 + num2;
         int resta = num1 - num2;
         int multiplicacion = num1 * num2;
         int division = num1 / num2;
         double divisionDecimal = (double) num1 / num2;
 
+        // Mostrar los resultados de las operaciones
         System.out.println("La suma de los numeros es: " + suma);
         System.out.println("La resta de los numeros es: " + resta);
         System.out.println("La multiplicacion de los numeros es: " + multiplicacion);
